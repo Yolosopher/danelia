dc25eb77a9904fb06d5044676bded8fc5b34e209
Nika Nishnianidze <61516311+Yolosopher@users.noreply.github.com>
2020-11-17 12:42:18 +0400

    respo-menu

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// variables
+const burger = document.querySelector('.burger');
+
+// functions
+
+
+// event listeners
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+})
