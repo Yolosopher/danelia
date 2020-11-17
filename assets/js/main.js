8e6e0708af1b0039ddb3283db119ddc858ca7f35
Nika Nishnianidze <61516311+Yolosopher@users.noreply.github.com>
2020-11-17 19:35:01 +0400

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 // variables
 const burger = document.querySelector('.burger');
+const homeMainArticles = document.querySelector('.home-main-section').querySelectorAll('article');
+const homeMainImages = document.querySelectorAll('.article-right-imgdiv');
+let a = [];
 
 // functions
 
@@ -7,4 +10,26 @@ const burger = document.querySelector('.burger');
 // event listeners
 burger.addEventListener('click', () => {
     burger.classList.toggle('active')
+})
+
+// load
+window.addEventListener('load', () => {
+    // article bg-color setter
+    homeMainArticles.forEach(element => {
+        let color = element.getAttribute('data-color');
+        element.style.backgroundColor = color;
+        let obj = {
+            elem: element.firstElementChild.lastElementChild,
+            color: color
+        };
+        a = [...a, obj];        
+    });
+    a.forEach(element => {
+        element.elem.addEventListener('mouseenter', () => {
+            element.elem.style.color = element.color;
+        })
+        element.elem.addEventListener('mouseleave', () => {
+            element.elem.style.color = '#fff';
+        })
+    })
 })
